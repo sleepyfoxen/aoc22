311158de462e1294e1933671159da2cdf650e42e
fox <fox@rawr.foxnet>
2022-12-08 16:30:14 +0000

    pain

--- day8.py
+++ day8.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from typing import Iterable, List
+from typing import List
 
 s = '''30373
 25512
@@ -34,3 +34,27 @@ for r, c in zip(rows, zip(*cols)):
     total += sum(r[i] or c[i] for i in range(len(rows)))
 
 print(total)
+
+
+def vis(i: int, s: List[int]) -> int:
+    count, t, i = 0, s[i], i - 1
+
+    while s[i] < t and i >= 0:
+        count += 1
+        i -= 1
+
+    if i >= 0: count += 1  # blocked by this tree
+    return count
+
+scores = []
+for i, r in enumerate(s):
+    for j, t in enumerate(r):
+        c = list(zip(*s))[j]
+
+        ltr, utd = vis(j, r), vis(i, c)
+        rtl = vis(len(r) - j - 1, r[::-1])
+        dtu = vis(len(c) - i - 1, c[::-1])
+        sc = ltr * rtl * utd * dtu
+        scores.append(sc)
+
+print(max(scores))
